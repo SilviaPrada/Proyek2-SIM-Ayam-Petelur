84274c47cc86135dbe32097a043a0abd55a9db0a
Milayn <millayunita07@gmail.com>
2022-12-07 16:36:23 +0700

    preparation backend

---
+++ app/Models/PenyakitAyam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PenyakitAyam extends Model
+{
+    use HasFactory;
+}

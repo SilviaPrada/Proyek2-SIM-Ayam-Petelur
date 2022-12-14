35687592af3b842897b2e4de76f62e824ac9d953
ShineDevi <shinedevi02@gmail.com>
2022-12-14 22:33:07 +0700

    membuat detail info

---
+++ resources/views/user/detailInfo.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SIMAP</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Ninestars - v4.9.1
+  * Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+   <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="logo">
+        <h1 class="text-light"><img src="assets/img/logo.png" class="img-fluid animated" alt=""></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+          <!-- <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li> -->
+          <!-- <li><a class="nav-link scrollto" href="#team">Team</a></li> -->
+          <li><a class="nav-link scrollto" href="#catat-ayam">Pencatatan Ayam</a></li>
+          <li><a class="nav-link scrollto" href="#hasil">Hasil Pencatatan</a></li>
+          <li><a class="nav-link scrollto" href="#hitung">Penghitungan</a></li>
+          <li><a class="nav-link scrollto active" href="#info">Informasi</a></li>
+          <li><a class="getstarted scrollto" href="#about">Keluar</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>
+  <!-- End Header -->
+  <!-- Content -->
+  <br><br>
+    <section id="detail" class="detail justify-content-center">
+        <div class="container">
+                <div class="row">
+                        <br><br><br>
+                        <div class="col-lg-12">
+                            <div class="title-all">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="title-all text-center">
+                                                <h3 style="font-weight: 700;">Gumboro</h3>
+                                                <br>
+                                                <img width="18%" src="assets/img/gumboro.png" alt="" />
+                                                <br><br>
+                                            <br>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 style="font-weight: 700;">Tentang Penyakit</h4><br>
+                                        <p style="padding-left: 25px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet ultrices est. </p>
+                                        <br>
+                                <br>
+                                <h4 style="font-weight: 700;">Gejala-Gejala</h4><br>
+                                        <p style="padding-left: 25px;">Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.</p>
+                                        <br>
+                                        <br>
+                                <h4 style="font-weight: 700;">Obat dan Penanganan</h4><br>
+                                        <p style="padding-left: 25px;">Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.</p>
+                                        <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </section>
+    
+  <!-- End Content -->
+   <!-- ======= Footer ======= -->
+   <footer id="footer">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+              <img src="assets/img/logo.png" class="img-fluid animated" alt="" height="300" width="100">
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <a href="#"><h4>Ketentuan Pemakaian</h4></a></li>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <a href="#"><h4>Privasi & Keamanan</h4></a></li>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="container py-4">
+        <div class="copyright">
+          &copy; 2022 <strong><span> Sistem Informasi Manajemen Ayam Petelur (SIMAP)</span></strong>
+        </div>
+    </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>

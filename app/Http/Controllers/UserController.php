63f6fd48988013ab53014d2553f579f1c137da72
Milayn <millayunita07@gmail.com>
2022-12-17 09:21:30 +0700

    add user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AnakKandang;
+use App\Models\BateraiKandang;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Crypt;
 
 class UserController extends Controller
 {
@@ -14,7 +18,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $user = User::all();
+        $user = $user->groupBy('role');
+        return view('admin.user.index', compact('user'));
     }
 
     /**
@@ -35,7 +41,39 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required',
+        ]);
+
+        if ($request->role == 'AnakKandang') {
+            $countAnakKandang = AnakKandang::where('no_ktp', $request->nomer)->count();
+            $anakKandangId = AnakKandang::where('no_ktp', $request->nomer)->get();
+            foreach ($anakKandangId as $val) {
+                $anakKandang = AnakKandang::findorfail($val->id);
+            }
+            if ($countAnakKandang >= 1) {
+                User::create([
+                    'name' => $anakKandang->nama_anak_kandang,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                    'role' => $request->role,
+                    'no_ktp' => $request->no_ktp,
+                ]);
+                return redirect()->back()->with('success', 'Berhasil menambahkan user Anak Kandang baru!');
+            } else {
+                return redirect()->back()->with('error', 'Maaf User ini tidak terdaftar sebagai Anak Kandang!');
+            }
+        } else {
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'role' => $request->role,
+            ]);
+            return redirect()->back()->with('success', 'Berhasil menambahkan user Admin baru!');
+        }
     }
 
     /**
@@ -44,9 +82,16 @@ class UserController extends Controller
      * @param  \App\Models\User  $User
      * @return \Illuminate\Http\Response
      */
-    public function show(User $User)
+    public function show(User $id)
     {
-        //
+        $id = Crypt::decrypt($id);
+        if ($id == "Admin" && Auth::user()->role == "Admin") {
+            return redirect()->back()->with('warning', 'Maaf halaman ini hanya bisa di akses oleh Admin!');
+        } else {
+            $user = User::where('role', $id)->get();
+            $role = $user->groupBy('role');
+            return view('admin.user.show', compact('user', 'role'));
+        }
     }
 
     /**
@@ -80,6 +125,225 @@ class UserController extends Controller
      */
     public function destroy(User $User)
     {
-        //
+        $user = User::findorfail($id);
+        if ($user->role == 'Admin') {
+            if ($user->id == Auth::user()->id) {
+                $user->delete();
+                return redirect()->back()->with('warning', 'Data user berhasil dihapus! (Silahkan cek trash data user)');
+            } else {
+                return redirect()->back()->with('error', 'Maaf user ini bukan milik anda!');
+            }
+        } else {
+            $user->delete();
+            return redirect()->back()->with('warning', 'Data user berhasil dihapus! (Silahkan cek trash data user)');
+        }
+    }
+
+    public function trash()
+    {
+        $user = User::onlyTrashed()->paginate(10);
+        return view('admin.user.trash', compact('user'));
+    }
+
+    public function restore($id)
+    {
+        $id = Crypt::decrypt($id);
+        $user = User::withTrashed()->findorfail($id);
+        $user->restore();
+        return redirect()->back()->with('info', 'Data user berhasil direstore! (Silahkan cek data user)');
+    }
+
+    public function kill($id)
+    {
+        $user = User::withTrashed()->findorfail($id);
+        $user->forceDelete();
+        return redirect()->back()->with('success', 'Data user berhasil dihapus secara permanent');
+    }
+
+    public function email(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        $countUser = User::where('email', $request->email)->count();
+        if ($countUser >= 1) {
+            return redirect()->route('reset.password', Crypt::encrypt($user->id))->with('success', 'Email ini sudah terdaftar!');
+        } else {
+            return redirect()->back()->with('error', 'Maaf email ini belum terdaftar!');
+        }
+    }
+
+    public function password($id)
+    {
+        $id = Crypt::decrypt($id);
+        $user = User::findorfail($id);
+        return view('auth.passwords.reset', compact('user'));
+    }
+
+    public function update_password(Request $request, $id)
+    {
+        $this->validate($request, [
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        $user = User::findorfail($id);
+        $user_data = [
+            'password' => Hash::make($request->password)
+        ];
+        $user->update($user_data);
+        return redirect()->route('login')->with('success', 'User berhasil diperbarui!');
+    }
+
+    public function profile()
+    {
+        return view('user.pengaturan');
+    }
+
+    public function edit_profile()
+    {
+        $baterai = BateraiKandang::all();
+        return view('user.profile', compact('baterai'));
+    }
+
+    public function ubah_profile(Request $request)
+    {
+        if ($request->role == 'AnakKandang') {
+            $this->validate($request, [
+                'nama_anak_kandang' => 'required',
+                'baterai_id' => 'required',
+                'jk' => 'required',
+            ]);
+            $anak_kandang = AnakKandang::where('id_card', Auth::user()->id_card)->first();
+            $user = User::where('id_card', Auth::user()->id_card)->first();
+            dd($user);
+            if ($user) {
+                $user_data = [
+                    'name' => $request->name
+                ];
+                $user->update($user_data);
+            } else {
+            }
+            $anak_kandang_data = [
+                'nama_anak_kandang' => $request->name,
+                'baterai_id' => $request->mapel_id,
+                'jk' => $request->jk,
+                'telp' => $request->telp,
+                'tmp_lahir' => $request->tmp_lahir,
+                'tgl_lahir' => $request->tgl_lahir
+            ];
+
+            
+            $anak_kandang->update($anak_kandang_data);
+            return redirect()->route('profile')->with('success', 'Profile anda berhasil diperbarui!');
+        }else {
+            $user = User::findorfail(Auth::user()->id);
+            $data_user = [
+                'name' => $request->name,
+            ];
+            $user->update($data_user);
+            return redirect()->route('profile')->with('success', 'Profile anda berhasil diperbarui!');
+        }
+    }
+
+    public function edit_foto()
+    {
+        if (Auth::user()->role == 'AnakKandang' || Auth::user()->role == 'Admin') {
+            return view('user.foto');
+        } else {
+            return redirect()->back()->with('error', 'Not Found 404!');
+        }
+    }
+
+    public function ubah_foto(Request $request)
+    {
+        if ($request->role == 'AnakKandang') {
+            $this->validate($request, [
+                'foto_anak_kandang' => 'required'
+            ]);
+            $anak_kandang = AnakKandang::where('id_card', Auth::user()->id_card)->first();
+            $foto_anak_kandang = $request->foto_anak_kandang;
+            $new_foto = date('s' . 'i' . 'H' . 'd' . 'm' . 'Y') . "_" . $foto->getClientOriginalName();
+            $anak_kandang_data = [
+                'foto_anak_kandang' => 'uploads/anak_kandang/' . $new_foto,
+            ];
+            $foto_anak_kandang->move('uploads/guru/', $new_foto);
+            $anak_kandang->update($anak_kandang_data);
+            return redirect()->route('profile')->with('success', 'Foto Profile anda berhasil diperbarui!');
+        } 
+    }
+
+    public function edit_email()
+    {
+        return view('user.email');
+    }
+
+    public function ubah_email(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string|email'
+        ]);
+        $user = User::findorfail(Auth::user()->id);
+        $cekUser = User::where('email', $request->email)->count();
+        if ($cekUser >= 1) {
+            return redirect()->back()->with('error', 'Maaf email ini sudah terdaftar!');
+        } else {
+            $user_email = [
+                'email' => $request->email,
+            ];
+            $user->update($user_email);
+            return redirect()->back()->with('success', 'Email anda berhasil diperbarui!');
+        }
+    }
+
+    public function edit_password()
+    {
+        return view('user.password');
+    }
+
+    public function ubah_password(Request $request)
+    {
+        $this->validate($request, [
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        $user = User::findorfail(Auth::user()->id);
+        if ($request->password_lama) {
+            if (Hash::check($request->password_lama, $user->password)) {
+                if ($request->password_lama == $request->password) {
+                    return redirect()->back()->with('error', 'Maaf password yang anda masukkan sama!');
+                } else {
+                    $user_password = [
+                        'password' => Hash::make($request->password),
+                    ];
+                    $user->update($user_password);
+                    return redirect()->back()->with('success', 'Password anda berhasil diperbarui!');
+                }
+            } else {
+                return redirect()->back()->with('error', 'Tolong masukkan password lama Anda dengan benar!');
+            }
+        } else {
+            return redirect()->back()->with('error', 'Tolong masukkan password lama Anda terlebih dahulu!');
+        }
+    }
+
+    public function cek_email(Request $request)
+    {
+        $countUser = User::where('email', $request->email)->count();
+        if ($countUser >= 1) {
+            return response()->json(['success' => 'Email Anda Benar']);
+        } else {
+            return response()->json(['error' => 'Maaf user not found!']);
+        }
+    }
+
+    public function cek_password(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        $countUser = User::where('email', $request->email)->count();
+        if ($countUser >= 1) {
+            if (Hash::check($request->password, $user->password)) {
+                return response()->json(['success' => 'Password Anda Benar']);
+            } else {
+                return response()->json(['error' => 'Maaf user not found!']);
+            }
+        } else {
+            return response()->json(['warning' => 'Maaf user not found!']);
+        }
     }
 }
